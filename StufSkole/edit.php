ab67fe824426cb6d078ec511497b5d28c0dce316
piehunter123 <34416605+piehunter123@users.noreply.github.com>
2022-10-26 10:46:06 +0200

    Changed Redirect URL

--- StufSkole/edit.php
+++ StufSkole/edit.php
@@ -13,5 +13,6 @@ $content=join("\n",$days);
 
 file_put_contents('test.txt', $content);
 //header("Expires: Mon, 20 Feb 2005 20:12:03 GMT"); //Didnt try this one yet, maybe it works better than js cache work-around? :)
-header('Location: http://tjemsland.rf.gd/demo');
+header('Location: https://htmlpreview.github.io/?https://raw.githubusercontent.com/odapaxal/StufProg/main/StufSkole/overview.html');
+//Yall might wanna change that url if you get an actual website domain from StUF :) Since it still redirects to the github preview page.
 ?>  
