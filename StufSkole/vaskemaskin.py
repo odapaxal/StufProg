75ac02cf2d9f19098f1f18c81180b0ba3892289d
odapaxal <oda.refsnes@gmail.com>
2023-01-18 14:52:00 +0100

    Oppdatering med virtual environment

---
+++ StufSkole/vaskemaskin.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+app.secret_key="Taylor Swift"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'odatabase'
+
+mysql=MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    msg=''
+    if request.method== 'POST' and 'username' in request.form and 'user_password' in request.form: #sjekker om brukernavn og passord variablene eksisterer i databasen
+        username = request.form['username']
+        password = request.form['user_password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute =('SELECT *FROM accounts WHERE username= %s AND user_password = %s',(username,user_password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['user_id'] = account['user_id']
+            session['username'] = account['username']
+            msg = 'Du er logget inn!'
+        else: msg ='Feil brukernavn/passord. Prøv igjen eller lag en ny bruker!'
+    return render_template('innlogging.html',msg=msg)
